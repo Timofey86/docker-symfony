7c5fb925f0fa15bbfce1113cbd0b0a69f11ed18c
Mukhin Timofey <muhintv@gmail.com>
2025-01-04 17:08:39 +0700

    6. CQRS (Query & Command Multiple Buses)

--- app/src/Users/Infrastructure/Repository/UserRepository.php
+++ app/src/Users/Infrastructure/Repository/UserRepository.php
@@ -22,4 +22,9 @@ class UserRepository extends ServiceEntityRepository implements UserRepositoryIn
     public function findByUlid(string $ulid): ?User{
         return $this->find($ulid);
     }
+
+    public function findByEmail(string $email): ?User
+    {
+        return $this->findOneBy(['email' => $email]);
+    }
 }
---
+++ app/tests/Functional/Users/Application/Query/FindUserByEmail/FindUserByEmailQueryHandlerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Functional\Users\Application\Query\FindUserByEmail;
+
+use App\Shared\Application\Query\QueryBusInterface;
+use App\Tests\Resource\Fixture\UserFixture;
+use App\Users\Application\DTO\UserDTO;
+use App\Users\Application\Query\FindUserByEmail\FindUserByEmailQuery;
+use App\Users\Application\Query\FindUserByEmail\FindUserByEmailQueryHandler;
+use App\Users\Domain\Repository\UserRepositoryInterface;
+
+use Faker\Factory;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class FindUserByEmailQueryHandlerTest extends WebTestCase
+{
+    public function setUp():void
+    {
+        parent::setUp();
+        $this->faker = Factory::create();
+        $this->queryBus = $this::getContainer()->get(QueryBusInterface::class);
+        $this->userRepository = $this::getContainer()->get(UserRepositoryInterface::class);
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
+    }
+
+    public function test_user_created_when_command_executed(): void
+    {
+        $referenceRepository = $this->databaseTool->loadFixtures([UserFixture::class])->getReferenceRepository();
+        $user = $referenceRepository->getReference(UserFixture::REFERENCE,\App\Users\Domain\Entity\User::class);
+        $query = new FindUserByEmailQuery($user->getEmail());
+
+        $userDTO = $this->queryBus->execute($query);
+
+        $this->assertInstanceOf(UserDTO::class, $userDTO);
+    }
+
+}
