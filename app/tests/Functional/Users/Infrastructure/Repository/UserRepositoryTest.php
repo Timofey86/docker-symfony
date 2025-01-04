121772e31d54c77a193fa247011c91ffa03c6bc4
Mukhin Timofey <muhintv@gmail.com>
2025-01-04 15:18:45 +0700

    5. Fixture

--- app/tests/Functional/Users/Infrastructure/Repository/UserRepositoryTest.php
+++ app/tests/Functional/Users/Infrastructure/Repository/UserRepositoryTest.php
@@ -2,10 +2,13 @@
 
 namespace App\Tests\Functional\Users\Infrastructure\Repository;
 
+use App\Tests\Resource\Fixture\UserFixture;
 use App\Users\Domain\Factory\UserFactory;
 use App\Users\Infrastructure\Repository\UserRepository;
 use Faker\Factory;
 use Faker\Generator;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserRepositoryTest extends WebTestCase
@@ -13,14 +16,18 @@ class UserRepositoryTest extends WebTestCase
    private UserRepository $repository;
    private Generator $faker;
 
+   private AbstractDatabaseTool $databaseTool;
+
     public function setUp(): void
     {
         parent::setUp();
         $this->repository = $this->getContainer()->get(UserRepository::class);
         $this->faker = Factory::create();
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
     }
 
-    public function test_user_added_successfully(): void{
+    public function test_user_added_successfully(): void
+    {
         $email = $this->faker->email();
         $password = $this->faker->password();
         $user = (new UserFactory())->create($email, $password);
@@ -28,6 +35,16 @@ class UserRepositoryTest extends WebTestCase
         $this->repository->add($user);
 
         $existingUser = $this->repository->findByUlid($user->getUlid());
+        $this->assertEquals($user->getUlid(), $existingUser->getUlid());
+    }
+
+    public function test_user_found_successfully(): void
+    {
+        $executor = $this->databaseTool->loadFixtures([UserFixture::class]);
+        $user = $executor->getReferenceRepository()->getReference(UserFixture::REFERENCE, \App\Users\Domain\Entity\User::class);
+
+        $existingUser = $this->repository->findByUlid($user->getUlid());
+
         $this->assertEquals($user->getUlid(), $existingUser->getUlid());
     }
 
---
+++ app/tests/Tools/FakerTools.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Tools;
+
+
+use Faker\Factory;
+use Faker\Generator;
+
+trait FakerTools
+{
+    public function getFaker(): Generator{
+        return Factory::create();
+    }
+
+}
